37fb24fa46ba3b533ca205a77e8afeab4adff62e
Viktoria <zaharova181114@gmail.com>
2022-07-01 23:00:28 +0300

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -154,12 +154,38 @@ $(window).scroll(function () {
 $('#schedule-successModal').modal('show');
 
 // this is your selector
-$('input.inp-file').MultiFile({
-    // your options go here
-    // max: 2,
-    accept: 'gif|jpg|png',
-    list: '.label-file-result',
-    STRING: {
-        remove: ''
-    }
+$.fileup({
+    url: window.location.pathname + window.location.search,
+    inputID: 'upload-demo',
+    queueID: 'upload-demo-queue',
+    dropzoneID: '',
+    files: [],
+    fieldName: 'filedata',
+    extraFields: {},
+    // lang: 'en',
+    sizeLimit: 0,
+    filesLimit: 0,
+    method: 'post',
+    timeout: null,
+    autostart: false,
+    // templateFile: '<div id="file-[INPUT_ID]-[FILE_NUM]" class="fileup-file [TYPE]">' +
+    //     '<div class="preview">' +
+    //     '<img src="[PREVIEW_SRC]" alt="[NAME]"/>' +
+    //     '</div>' +
+    //     '<div class="data">' +
+    //     '<div class="description">' +
+    //     '<span class="file-name">[NAME]</span> (<span class="file-size">[SIZE_HUMAN]</span>)' +
+    //     '</div>' +
+    //     '<div class="controls">' +
+    //     '<span class="remove" onclick="$.fileup(\'[INPUT_ID]\', \'remove\', \'[FILE_NUM]\');" title="[REMOVE]">x</span>' +
+    //     '<span class="upload" onclick="$.fileup(\'[INPUT_ID]\', \'upload\', \'[FILE_NUM]\');">[UPLOAD]</span>' +
+    //     '<span class="abort" onclick="$.fileup(\'[INPUT_ID]\', \'abort\', \'[FILE_NUM]\');" style="display:none">[ABORT]</span>' +
+    //     '</div>' +
+    //     '<div class="result"></div>' +
+    //     '<div class="fileup-progress">' +
+    //     '<div class="fileup-progress-bar"></div>' +
+    //     '</div>' +
+    //     '</div>' +
+    //     '<div class="clear">x</div>' +
+    //     '</div>',
 });
